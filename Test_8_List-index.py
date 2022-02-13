73d92ac30e82d414d0b3e599f65057f38d1dd100
--replace-all <manishbirthliya@gmail.com>
2022-02-13 15:54:23 +0530

    8 updated

--- Test_8_List-index.py
+++ Test_8_List-index.py
@@ -2,9 +2,12 @@
 a = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
 
 print(a[0])
-print(a[1])
+# print(a[1])
+# print(a[3])
+# print(a[4])
+# print(a[5])
+print(a)
 print(a[2])
-print(a[3])
-print(a[4])
-print(a[5])
+a[0]=100
+print(a)
 #ends
