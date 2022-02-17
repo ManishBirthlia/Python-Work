cc3d893e4b82747a8131ce783615fff5b29c1661
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2022-02-17 15:18:07 +0530

    Add files via upload

---
+++ Test_9_01.py
@@ -0,0 +1,19 @@
+# 1st method
+from re import I
+
+
+b=input("input: ")
+c=input("input: ")
+d=input("input: ")
+e=input("input: ")
+g=input("input: ")
+h=input("input: ")
+i=input("input: ")
+f=input("input: ")
+j=input("input: ")
+a=[b,c,d,e,f,g,h,i,j]
+# 2nd method
+# a=[]
+# b=input("input:\n")
+# a.append(b)
+# print(a)
