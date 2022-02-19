d011ebd5b2757d79b38a1c36f5309cf98044d97b
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2022-02-19 20:05:31 +0530

    Add files via upload

--- Test_4.py
+++ Test_4.py
@@ -2,9 +2,10 @@ from asyncore import loop
 from tracemalloc import start
 
 from pip import main
-
+# taking input from user
 a=input("Input :")
 print(type(a))
+# this is small function i made to find value at given index
 # x=0
 # print(a[x])
 # x+=1
@@ -20,5 +21,5 @@ print(type(a))
 # x+=1
 x=int(input('take any number from 0 onward '))
 print(a[x])
-print(a[2:x])
+print(a[2:x])# here x in excluded or simply not taken in python
 print(a[x:10])
