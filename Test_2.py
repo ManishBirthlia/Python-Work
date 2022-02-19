0820fac98657520940c4c13843bc729a489d21e7
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2022-02-19 20:05:09 +0530

    Add files via upload

--- Test_2.py
+++ Test_2.py
@@ -1,7 +1,8 @@
-
+#simple arthmetic properties
 a=2
 b=2.2
 c="this is string"
+# print(a+c) #this will throw error a=int but c=string
 print("a+b is :",a+b)
 print("a*b is :",a*b)
 print("a/b is :",a/b)
