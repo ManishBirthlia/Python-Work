db7bae92bc92fc0e58e93a89c64d8263a83462a3
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2022-02-19 20:05:21 +0530

    Add files via upload

--- Test_3.py
+++ Test_3.py
@@ -1,7 +1,17 @@
+# taking input from user
 a=int(input('put any number of a: '))
 b=int(input('put any number of b: '))
+# performing arthmetic operation
 print('a+b is:',a+b)
 print('a-b is:',a-b)
 print('a*b is:',a*b)
 print('a/b is:',a/b)
 print('Reminder of a/b is:',a%b)
+# you can also overwrite variables
+a=2
+print(a)
+a=float(a)
+print(a)
+print(type(a))
+# for begin there are 3 types of variables 
+# int , float & string 
