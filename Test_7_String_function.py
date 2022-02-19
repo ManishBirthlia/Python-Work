ed65c2960e84e4bbf84235214b2121bc98d61acc
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2022-02-19 20:06:01 +0530

    Add files via upload

--- Test_7_String_function.py
+++ Test_7_String_function.py
@@ -1,11 +1,11 @@
 #taking an input
 a=input('type any sting:\n')
 #all the sting function
-print(len(a)) #counts all the letters in the sting
+print(len(a)) # len(veriable) tell us length of the variable
 print(a.endswith("")) #finds the end word in the sting
 print(a.replace("manish","sunil")) #replace all the manish letters with sunil in the sting
 print(a.count("a")) #counts all the "a" in the sting
 print(a.capitalize()) #capitalize the 1st word in the sting
-print(a.find("")) #to find the position of the letters or word in the sting
+print(a.find("")) #to find the position of the letters or word in the sting in terms of index
 
 #end
