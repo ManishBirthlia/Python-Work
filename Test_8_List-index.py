547aa52023fc66d0cf6bf6e6a91abf05a9c1cc34
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2022-02-19 20:06:14 +0530

    Add files via upload

--- Test_8_List-index.py
+++ Test_8_List-index.py
@@ -1,6 +1,6 @@
-# input
-a = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
-
+# making variables
+a = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20] # all the variables starts with [] is a list
+# properties same as string
 print(a[0])
 # print(a[1])
 # print(a[3])
@@ -8,7 +8,7 @@ print(a[0])
 # print(a[5])
 print(a)
 print(a[2])
-a[0]=100
+a[0]=100 #list can be ultered
 print(a)
 print(a[0:5])
 #ends
