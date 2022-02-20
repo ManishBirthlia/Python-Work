c3314d56fe3f2423903f3154a6278b8bc7c1617f
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2022-02-21 00:36:54 +0530

    Add files via upload

---
+++ Test_13_conditional.py
@@ -0,0 +1,15 @@
+
+# a=[]
+# b=int(input('take any number >0 :\n'))
+# a.append(b)
+# if (b>0):
+#     print(a)
+# else:
+#     print('take b>0 (ERROR)')
+
+
+a=int(input('geuss the age of the user and it is greater then 18 :'))
+if(a>=18):
+    print( a ,'yes it is greater then or equal to 18')
+else:
+    print('No you are wrong')
