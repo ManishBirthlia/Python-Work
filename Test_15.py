3be0758be3c0fe4ad44b9a7eb3654229c4be3981
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2022-02-21 12:23:11 +0530

    Delete Test_15.py

--- Test_15.py
+++
@@ -1,8 +0,0 @@
-a=int(input('take 1st number:\n'))
-b=int(input('take 2nd number:\n'))
-c=int(input('take 3rd number:\n'))
-
-if(a<33 or b<33 or c<33) or ((a+b+c)/3<40):
-    print('you are fail')
-else:
-    print('you are pass')
