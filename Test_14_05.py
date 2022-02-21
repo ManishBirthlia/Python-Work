a9713fc48d03db88416296dbd31623f824ca2ee7
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2022-02-21 12:25:15 +0530

    Add files via upload

---
+++ Test_14_05.py
@@ -0,0 +1,9 @@
+# taking inputs
+b=input('write any name which you think is present in List:\n')
+# this is list
+a=['manish','sunil','harry','monu']
+
+if(b in a):
+    print('yes this name is present in List')
+else:
+    print('no there is no such name present in List')
