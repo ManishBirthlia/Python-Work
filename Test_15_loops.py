74dad750dcf487ff5647cad9f6771ce9e6c575f7
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2022-02-21 23:12:17 +0530

    Add files via upload

---
+++ Test_15_loops.py
@@ -0,0 +1,12 @@
+
+
+li=['sa','re','ga','ma','pa','da','ni','sa']
+print(len(li))
+# by using while loop
+i=0
+while i<len(li):
+    print(i+1,'member of list is',li[i])
+    i=i+1
+# by using for loop
+for a in li:
+    print(a)
