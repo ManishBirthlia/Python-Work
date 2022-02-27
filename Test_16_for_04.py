6ffaa463be0b74f468f7feb7365c6d9623af48b2
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2022-02-27 23:29:15 +0530

    Add files via upload

---
+++ Test_16_for_04.py
@@ -0,0 +1,5 @@
+li=[]
+for i in range(int(input('how many number of elements you want to add in this list ? \n'))):
+    li.append(input('input item \n'))
+else:
+    print("this is your modified list :-",li)
