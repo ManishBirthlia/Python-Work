f1f23285068f6e26262aa805b5f06d7d602e6bdb
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2022-02-28 23:59:50 +0530

    Add files via upload

---
+++ Test_16_for_09.py
@@ -0,0 +1,17 @@
+num=int(input('\t take any number\n'))
+
+# if num==1:
+#     print('*')
+a="*"
+for i in range(num):
+    # if num == 1:
+    #     break
+    print('\t',a)
+    a=a+"*"
+
+# a="*"*num
+# for i in range(num):
+#     if num == 1:
+#         break
+#     print('\t',a)
+#     a=a-"*"
