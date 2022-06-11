8b2a36daee3bd790c0d3d86943ff87a65f4204fc
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2022-06-11 12:15:36 +0530

    Add files via upload

--- Test_9_List-Methods.py
+++ Test_9_List-Methods.py
@@ -1,5 +1,11 @@
 #inputs
 a=[2,1,4,3,7,0,5,8,6,3]
 print(a)
-a.sort()# it will sort number in increasing order
+a.sort()# it will sort list in increasing order
+a.reverse()# it will sort list in decreseing order
+a.append(6)# it will add number in list at the end  
+a.index(6)# it will tell number at index 6  
+a.pop(3)# it will remove number at index 3  
+a.insert(3,5)# it will add number at an index 
+a.remove(4)# it will remove the number in a list 
 print(a)
